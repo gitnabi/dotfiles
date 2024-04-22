bfb71836c90b990d9b749cc07c3e8d13e366d187
gitnabi <47212809+gitnabi@users.noreply.github.com>
2024-04-22 22:36:17 +0300

    Update ubuntu_setting_up_shell.sh

--- ubuntu_setting_up_shell.sh
+++ ubuntu_setting_up_shell.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -x
 
-sudo apt-get update > /dev/null 2>&1
-sudo apt-get install -y zsh git curl gawk > /dev/null 2>&1
+sudo apt-get update > /dev/null
+sudo apt-get install -y zsh git curl gawk > /dev/null
 sudo chsh -s $(which zsh) $USER
 
-printf "Y\n" | (sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)") > /dev/null 2>&1
+printf "Y\n" | (sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)") > /dev/null
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k > /dev/null 2>&1
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k > /dev/null
 gawk -i inplace '{gsub(/ZSH_THEME="robbyrussell"/,"# ZSH_THEME=\"robbyrussell\"\nZSH_THEME=\"powerlevel10k/powerlevel10k\"")}1' ~/.zshrc
 
-git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting > /dev/null 2>&1
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting > /dev/null
 sed -i 's/\(plugins=(.*[^)]\))/\1 zsh-syntax-highlighting)/' ~/.zshrc
 
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions > /dev/null 2>&1
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions > /dev/null
 sed -i 's/\(plugins=(.*[^)]\))/\1 zsh-autosuggestions)/' ~/.zshrc
 
 zsh -c "source ~/.zshrc"
